5abf53b87bc43672efd1b28d4fd7ff91efeb15af
Aleksey <Aleksey@alekseys-mac.local>
2016-06-19 17:48:33 +0300

    Initial Commit

---
+++ ArticleViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArticleViewer
+//
+//  Created by Aleksey on 6/19/16.
+//  Copyright © 2016 Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
